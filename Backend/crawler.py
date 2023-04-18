fef8b13a96802373716845f762186c2d954b7691
spoeker <nhat-duc@web.de>
2023-04-18 20:27:37 +0200

    Tonie class erstellt

---
+++ Backend/crawler.py
@@ -0,0 +1,3 @@
+class Crawler():
+    def crawl(self):
+        url= "https://tonies.com/de-de/tonies/"
---
+++ Backend/tonie_class.py
@@ -0,0 +1,14 @@
+class Tonie ():
+    def __init__(self, title, figure, description, titlelist, runtime, age_recommendation, material, image):
+        self.title=title
+        self.figure= figure
+        self.descripton=description 
+        self.titlelist=titlelist
+        self.runtime=runtime 
+        self.age_recommendation=age_recommendation
+        self.material=material 
+        self.image=image
+
+
+
+
